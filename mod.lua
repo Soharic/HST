86d9cc7d5cb54c58ca60381dcd0815043b148b1f
Soharic <42074772+Soharic@users.noreply.github.com>
2018-08-04 17:28:47 +0100

    Add files via upload
    v1.7 as currently available on the Steam Workshop. Last updated 07/12/16

---
+++ mod.lua
@@ -0,0 +1,26 @@
+﻿require "tableutil"
+
+function data()
+return {
+	info = {
+	minorVersion = 0,
+	severityAdd = "NONE",
+	severityRemove = "NONE",
+	name = _("British Rail Class 43 HST"),
+	description = _("Intercity Swallow liveried British High Speed Trains composed of Class 43 diesel locomotives and Mk3 coaching stock. This mod comes with two variants, the standard 8x Mk3 Coaches topped and tailed with 2x Class 43s (220m long) and the shortend version comprising 5x Mk3 Coaches topped and tailed with 2x Class 43s (151m long and in a formation sometimes used by Cross Country today).\nThe Mk3 coaches will not be released on their own as the models are a little too small - this is not noticable when they are kept in set formations but would be if mixed with other locomotives.\n \nUnlocked 1975-1983\nMaximum Speed 201kp/h\nCapacity 76 per Mk3 coach\nLifespan 45 years\n \nModels by mfurseman for Train Fever\nConverted to Transport Fever by 9th Wonder with thanks to killakanz"),
+	authors = { -- OPTIONAL one or multiple authors
+		{
+			name = "9th Wonder", -- author name
+			role = "CREATOR", -- OPTIONAL "CREATOR", "CO_CREATOR", "TESTER" or "BASED_ON" or "OTHER"
+			tfnetId = "" -- OPTIONAL train-fever-net author id
+		},		
+		{
+		name = "mfurseman", -- author name
+		role = "BASED_ON", -- OPTIONAL "CREATOR", "CO_CREATOR", "TESTER" or "BASED_ON" or "OTHER"
+		tfnetId = "" -- OPTIONAL train-fever-net author id
+		}
+	},
+	tags = { "locomotive", "diesel", "vehicle", "train", "passenger", "set", "British", "Europe" },
+	},
+}
+end
---
+++ res/config/multiple_unit/hst_2.lua
@@ -0,0 +1,18 @@
+function data()
+return {
+	vehicles = {
+		{ name = "vehicle/train/class_43_0.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3.mdl", forward = true },
+		{ name = "vehicle/train/class_43_1.mdl", forward = true },
+	},
+	name = _("HST"),
+	desc = _("The British High Speed Train topped and tailed by Class 43 power cars.")
+}
+end
