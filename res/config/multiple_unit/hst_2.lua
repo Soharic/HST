d707abc7443e2916c551597b6629fb90cf5bab25
Soharic <42074772+Soharic@users.noreply.github.com>
2018-11-25 01:25:32 +0000

    Update hst_2.lua

--- res/config/multiple_unit/hst_2.lua
+++ res/config/multiple_unit/hst_2.lua
@@ -12,7 +12,7 @@ return {
 		{ name = "vehicle/waggon/br_mk3.mdl", forward = true },
 		{ name = "vehicle/train/class_43_1.mdl", forward = true },
 	},
-	name = _("HST"),
-	desc = _("The British High Speed Train topped and tailed by Class 43 power cars.")
+	name = _("HST (2+8)"),
+	desc = _("Eight Mk3 coaches in Intercity Livery topped and tailed by Class 43 power cars.")
 }
 end
