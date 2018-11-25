731d879016c1558d4188abac2996d345b65b5177
Soharic <42074772+Soharic@users.noreply.github.com>
2018-11-25 01:38:36 +0000

    Update gner_2.lua

--- res/config/multiple_unit/gner_2.lua
+++ res/config/multiple_unit/gner_2.lua
@@ -13,7 +13,7 @@ return {
 		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
 		{ name = "vehicle/train/class_43_gner_1.mdl", forward = true },
 	},
-	name = _("GNER HST (2 + 9)"),
-	desc = _("The British High Speed Train topped and tailed by Class 43 power cars.")
+	name = _("GNER HST (2+9)"),
+	desc = _("Nine Mk3 coaches in GNER Livery topped and tailed by Class 43 power cars.")
 }
 end
