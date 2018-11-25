980d0da5857593a8b6d9bcbb4da99b4d39c1ec56
Soharic <42074772+Soharic@users.noreply.github.com>
2018-11-25 09:57:12 +0000

    Update hst_4.lua

--- res/config/multiple_unit/hst_4.lua
+++ res/config/multiple_unit/hst_4.lua
@@ -1,17 +1,17 @@
 function data()
 return {
 	vehicles = {
-		{ name = "vehicle/train/class_43_gner_0.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
-		{ name = "vehicle/train/class_43_gner_1.mdl", forward = true },
+		{ name = "vehicle/train/gner_class_43_0.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/train/gner_class_43_1.mdl", forward = true },
 	},
 	name = _("GNER HST (2+9)"),
 	desc = _("Nine Mk3 coaches in GNER Livery topped and tailed by Class 43 power cars.")
