591a0867e65e56e281663a3d65b427aba18c11f9
Soharic <42074772+Soharic@users.noreply.github.com>
2018-11-25 01:26:37 +0000

    Add files via upload

---
+++ res/config/multiple_unit/gner_2.lua
@@ -0,0 +1,19 @@
+function data()
+return {
+	vehicles = {
+		{ name = "vehicle/train/class_43_gner_0.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gner.mdl", forward = true },
+		{ name = "vehicle/train/class_43_gner_1.mdl", forward = true },
+	},
+	name = _("GNER HST (2 + 9)"),
+	desc = _("The British High Speed Train topped and tailed by Class 43 power cars.")
+}
+end
