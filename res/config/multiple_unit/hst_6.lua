18f3ad68812ae0725ea827e8cbb51dd3556c82f2
Soharic <42074772+Soharic@users.noreply.github.com>
2018-11-25 18:29:08 +0000

    Add files via upload
    All new files except mdl

---
+++ res/config/multiple_unit/hst_6.lua
@@ -0,0 +1,18 @@
+function data()
+return {
+	vehicles = {
+		{ name = "vehicle/train/class_43_gwr_0.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
+		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
+		{ name = "vehicle/train/class_43_gwr_1.mdl", forward = true },
+	},
+	name = _("Great Western Railway HST (2 + 8)"),
+	desc = _("The British High Speed Train topped and tailed by Class 43 power cars.")
+}
+end
