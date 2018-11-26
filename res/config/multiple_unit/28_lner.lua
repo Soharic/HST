32637eef798798dfe5a8172589346c86163bfecd
Soharic <42074772+Soharic@users.noreply.github.com>
2018-11-26 11:00:33 +0000

    Add files via upload

---
+++ res/config/multiple_unit/28_lner.lua
@@ -0,0 +1,18 @@
+function data()
+return {
+	vehicles = {
+		{ name = "vehicle/train/lner_class_43_0.mdl", forward = true },
+		{ name = "vehicle/waggon/lner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/lner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/lner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/lner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/lner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/lner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/lner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/lner_mk3.mdl", forward = true },
+		{ name = "vehicle/train/lner_class_43_1.mdl", forward = true },
+	},
+	name = _("HST (2+8)"),
+	desc = _("Eight Mk3 coaches in LNER Livery topped and tailed by Class 43 power cars.")
+}
+end
