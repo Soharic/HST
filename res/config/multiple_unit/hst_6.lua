46bfb4a486918139768fef0f12b26263def251bb
Soharic <42074772+Soharic@users.noreply.github.com>
2018-11-26 10:27:53 +0000

    Update file references
    Updated files references to refer to correct file name

--- res/config/multiple_unit/hst_6.lua
+++ res/config/multiple_unit/hst_6.lua
@@ -1,18 +1,18 @@
 function data()
 return {
 	vehicles = {
-		{ name = "vehicle/train/class_43_gwr_0.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
-		{ name = "vehicle/waggon/br_mk3_gwr.mdl", forward = true },
-		{ name = "vehicle/train/class_43_gwr_1.mdl", forward = true },
+		{ name = "vehicle/train/gwr_class_43_0.mdl", forward = true },
+		{ name = "vehicle/waggon/gwr_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gwr_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gwr_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gwr_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gwr_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gwr_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gwr_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gwr_mk3.mdl", forward = true },
+		{ name = "vehicle/train/gwr_class_43_1.mdl", forward = true },
 	},
-	name = _("Great Western Railway HST (2 + 8)"),
-	desc = _("The British High Speed Train topped and tailed by Class 43 power cars.")
+	name = _("HST (2+8)"),
+	desc = _("Eight Mk3 coaches in GWR Livery topped and tailed by Class 43 power cars.")
 }
 end
