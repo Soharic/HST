9cab82410eee47d9d3dd04250d99b53149fdf40f
Soharic <42074772+Soharic@users.noreply.github.com>
2018-11-27 14:11:23 +0000

    Update hst_1.lua

--- res/config/multiple_unit/hst_1.lua
+++ res/config/multiple_unit/hst_1.lua
@@ -1,15 +1,15 @@
 function data()
 return {
 	vehicles = {
-		{ name = "vehicle/train/class_43_0.mdl", forward = true },
+		{ name = "vehicle/train/br_class_43_0.mdl", forward = true },
 		{ name = "vehicle/waggon/br_mk3.mdl", forward = true },
 		{ name = "vehicle/waggon/br_mk3.mdl", forward = true },
 		{ name = "vehicle/waggon/br_mk3.mdl", forward = true },
 		{ name = "vehicle/waggon/br_mk3.mdl", forward = true },
 		{ name = "vehicle/waggon/br_mk3.mdl", forward = true },
-		{ name = "vehicle/train/class_43_1.mdl", forward = true },
+		{ name = "vehicle/train/br_class_43_1.mdl", forward = true },
 	},
-	name = _("HST (short)"),
-	desc = _("The British High Speed Train topped and tailed by Class 43 power cars.")
+	name = _("HST (2+5)"),
+	desc = _("Five Mk3 coaches in Intercity Livery topped and tailed by Class 43 power cars.")
 }
 end
