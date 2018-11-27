ceb3cf47f23405b7740f68b09d93369517aaf94f
Soharic <42074772+Soharic@users.noreply.github.com>
2018-11-27 14:13:32 +0000

    Create hst_gner_28.lua

---
+++ res/config/multiple_unit/hst_gner_28.lua
@@ -0,0 +1,18 @@
+function data()
+return {
+	vehicles = {
+		{ name = "vehicle/train/gner_class_43_0.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/waggon/gner_mk3.mdl", forward = true },
+		{ name = "vehicle/train/gner_class_43_1.mdl", forward = true },
+	},
+	name = _("GNER HST (2+8)"),
+	desc = _("Eight Mk3 coaches in GNER Livery topped and tailed by Class 43 power cars.")
+}
+end
